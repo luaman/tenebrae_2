f18bde32ee99c493a392d65d358150f50787df3c
cholleme <cholleme>
2003-02-03 13:58:48 +0000

    Added basic shader parsing Moved some extension definitions to glNV.h (shared between nvparse and tenebrae)

--- glquake.h
+++ glquake.h
@@ -388,6 +388,8 @@ extern  cvar_t	fog_start;
 extern  cvar_t	fog_end;
 extern  cvar_t	gl_fog;
 extern	float	fog_color[4];
+extern	cvar_t	r_tangentscale; //scale tangent/binormal by this
+
 
 extern	int			mirrortexturenum;	// quake texturenum, not gltexturenum
 extern	qboolean	mirror;
@@ -427,43 +429,6 @@ void GL_Bind (int texnum);
 // Multitexture
 //#define    TEXTURE0_SGIS				0x835E
 //#define    TEXTURE1_SGIS				0x835F
-
-/* ARB_multitexture defines and prototypes from <GL/gl.h> */
-#define GL_ACTIVE_TEXTURE_ARB               0x84E0
-#define GL_CLIENT_ACTIVE_TEXTURE_ARB        0x84E1
-#define GL_MAX_ACTIVE_TEXTURES_ARB          0x84E2
-#define GL_TEXTURE0_ARB                     0x84C0
-#define GL_TEXTURE1_ARB                     0x84C1
-#define GL_TEXTURE2_ARB                     0x84C2
-#define GL_TEXTURE3_ARB                     0x84C3
-#define GL_TEXTURE4_ARB                     0x84C4
-#define GL_TEXTURE5_ARB                     0x84C5
-#define GL_TEXTURE6_ARB                     0x84C6
-#define GL_TEXTURE7_ARB                     0x84C7
-#define GL_TEXTURE8_ARB                     0x84C8
-#define GL_TEXTURE9_ARB                     0x84C9
-#define GL_TEXTURE10_ARB                    0x84CA
-#define GL_TEXTURE11_ARB                    0x84CB
-#define GL_TEXTURE12_ARB                    0x84CC
-#define GL_TEXTURE13_ARB                    0x84CD
-#define GL_TEXTURE14_ARB                    0x84CE
-#define GL_TEXTURE15_ARB                    0x84CF
-#define GL_TEXTURE16_ARB                    0x84D0
-#define GL_TEXTURE17_ARB                    0x84D1
-#define GL_TEXTURE18_ARB                    0x84D2
-#define GL_TEXTURE19_ARB                    0x84D3
-#define GL_TEXTURE20_ARB                    0x84D4
-#define GL_TEXTURE21_ARB                    0x84D5
-#define GL_TEXTURE22_ARB                    0x84D6
-#define GL_TEXTURE23_ARB                    0x84D7
-#define GL_TEXTURE24_ARB                    0x84D8
-#define GL_TEXTURE25_ARB                    0x84D9
-#define GL_TEXTURE26_ARB                    0x84DA
-#define GL_TEXTURE27_ARB                    0x84DB
-#define GL_TEXTURE28_ARB                    0x84DC
-#define GL_TEXTURE29_ARB                    0x84DD
-#define GL_TEXTURE30_ARB                    0x84DE
-#define GL_TEXTURE31_ARB                    0x84DF
 
 // ARB_texture_compression defines
 #define GL_COMPRESSED_RGBA_ARB                0x84EE
@@ -528,7 +493,6 @@ extern PFNGLMULTITEXCOORD3FVARBPROC qglMultiTexCoord3fvARB;
 // <AWE> : MacOS X 10.2: defined in <OpenGL/glext.h>
 #if !defined (__APPLE__) && !defined (MACOSX) 
 
-//PENTA: Texture env combine/ Texture env dot3
 #define GL_COMBINE_ARB                    0x8570
 #define GL_COMBINE_RGB_ARB                0x8571
 #define GL_COMBINE_ALPHA_ARB              0x8572
@@ -594,96 +558,14 @@ extern PFNGLMULTITEXCOORD3FVARBPROC qglMultiTexCoord3fvARB;
 #define GL_PROXY_TEXTURE_CUBE_MAP_ARB     0x851B
 #define GL_MAX_CUBE_MAP_TEXTURE_SIZE_ARB  0x851C
 
-//PENTA: NV register combiners
-#define GL_REGISTER_COMBINERS_NV          0x8522
-#define GL_VARIABLE_A_NV                  0x8523
-#define GL_VARIABLE_B_NV                  0x8524
-#define GL_VARIABLE_C_NV                  0x8525
-#define GL_VARIABLE_D_NV                  0x8526
-#define GL_VARIABLE_E_NV                  0x8527
-#define GL_VARIABLE_F_NV                  0x8528
-#define GL_VARIABLE_G_NV                  0x8529
-#define GL_CONSTANT_COLOR0_NV             0x852A
-#define GL_CONSTANT_COLOR1_NV             0x852B
-#define GL_PRIMARY_COLOR_NV               0x852C
-#define GL_SECONDARY_COLOR_NV             0x852D
-#define GL_SPARE0_NV                      0x852E
-#define GL_SPARE1_NV                      0x852F
-#define GL_DISCARD_NV                     0x8530
-#define GL_E_TIMES_F_NV                   0x8531
-#define GL_SPARE0_PLUS_SECONDARY_COLOR_NV 0x8532
-#define GL_UNSIGNED_IDENTITY_NV           0x8536
-#define GL_UNSIGNED_INVERT_NV             0x8537
-#define GL_EXPAND_NORMAL_NV               0x8538
-#define GL_EXPAND_NEGATE_NV               0x8539
-#define GL_HALF_BIAS_NORMAL_NV            0x853A
-#define GL_HALF_BIAS_NEGATE_NV            0x853B
-#define GL_SIGNED_IDENTITY_NV             0x853C
-#define GL_SIGNED_NEGATE_NV               0x853D
-#define GL_SCALE_BY_TWO_NV                0x853E
-#define GL_SCALE_BY_FOUR_NV               0x853F
-#define GL_SCALE_BY_ONE_HALF_NV           0x8540
-#define GL_BIAS_BY_NEGATIVE_ONE_HALF_NV   0x8541
-#define GL_COMBINER_INPUT_NV              0x8542
-#define GL_COMBINER_MAPPING_NV            0x8543
-#define GL_COMBINER_COMPONENT_USAGE_NV    0x8544
-#define GL_COMBINER_AB_DOT_PRODUCT_NV     0x8545
-#define GL_COMBINER_CD_DOT_PRODUCT_NV     0x8546
-#define GL_COMBINER_MUX_SUM_NV            0x8547
-#define GL_COMBINER_SCALE_NV              0x8548
-#define GL_COMBINER_BIAS_NV               0x8549
-#define GL_COMBINER_AB_OUTPUT_NV          0x854A
-#define GL_COMBINER_CD_OUTPUT_NV          0x854B
-#define GL_COMBINER_SUM_OUTPUT_NV         0x854C
-#define GL_MAX_GENERAL_COMBINERS_NV       0x854D
-#define GL_NUM_GENERAL_COMBINERS_NV       0x854E
-#define GL_COLOR_SUM_CLAMP_NV             0x854F
-#define GL_COMBINER0_NV                   0x8550
-#define GL_COMBINER1_NV                   0x8551
-#define GL_COMBINER2_NV                   0x8552
-#define GL_COMBINER3_NV                   0x8553
-#define GL_COMBINER4_NV                   0x8554
-#define GL_COMBINER5_NV                   0x8555
-#define GL_COMBINER6_NV                   0x8556
-#define GL_COMBINER7_NV                   0x8557
-
 #define GL_TEXTURE_MAX_ANISOTROPY_EXT		0x84FE
 #define GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT	0x84FF
 
 
 #endif /* !__APPLE__ && !MACOSX */
 
-// - DC - 
-#if !defined(SDL) && !defined (__glx__)
-typedef void (APIENTRY * PFNGLCOMBINERPARAMETERFVNVPROC) (GLenum pname, const GLfloat *params);
-typedef void (APIENTRY * PFNGLCOMBINERPARAMETERFNVPROC) (GLenum pname, GLfloat param);
-typedef void (APIENTRY * PFNGLCOMBINERPARAMETERIVNVPROC) (GLenum pname, const GLint *params);
-typedef void (APIENTRY * PFNGLCOMBINERPARAMETERINVPROC) (GLenum pname, GLint param);
-typedef void (APIENTRY * PFNGLCOMBINERINPUTNVPROC) (GLenum stage, GLenum portion, GLenum variable, GLenum input, GLenum mapping, GLenum componentUsage);
-typedef void (APIENTRY * PFNGLCOMBINEROUTPUTNVPROC) (GLenum stage, GLenum portion, GLenum abOutput, GLenum cdOutput, GLenum sumOutput, GLenum scale, GLenum bias, GLboolean abDotProduct, GLboolean cdDotProduct, GLboolean muxSum);
-typedef void (APIENTRY * PFNGLFINALCOMBINERINPUTNVPROC) (GLenum variable, GLenum input, GLenum mapping, GLenum componentUsage);
-typedef void (APIENTRY * PFNGLGETCOMBINERINPUTPARAMETERFVNVPROC) (GLenum stage, GLenum portion, GLenum variable, GLenum pname, GLfloat *params);
-typedef void (APIENTRY * PFNGLGETCOMBINERINPUTPARAMETERIVNVPROC) (GLenum stage, GLenum portion, GLenum variable, GLenum pname, GLint *params);
-typedef void (APIENTRY * PFNGLGETCOMBINEROUTPUTPARAMETERFVNVPROC) (GLenum stage, GLenum portion, GLenum pname, GLfloat *params);
-typedef void (APIENTRY * PFNGLGETCOMBINEROUTPUTPARAMETERIVNVPROC) (GLenum stage, GLenum portion, GLenum pname, GLint *params);
-typedef void (APIENTRY * PFNGLGETFINALCOMBINERINPUTPARAMETERFVNVPROC) (GLenum variable, GLenum pname, GLfloat *params);
-typedef void (APIENTRY * PFNGLGETFINALCOMBINERINPUTPARAMETERIVNVPROC) (GLenum variable, GLenum pname, GLint *params);
-#endif
 
-extern PFNGLCOMBINERPARAMETERFVNVPROC qglCombinerParameterfvNV;
-extern PFNGLCOMBINERPARAMETERIVNVPROC qglCombinerParameterivNV;
-extern PFNGLCOMBINERPARAMETERFNVPROC qglCombinerParameterfNV;
-extern PFNGLCOMBINERPARAMETERINVPROC qglCombinerParameteriNV;
-extern PFNGLCOMBINERINPUTNVPROC qglCombinerInputNV;
-extern PFNGLCOMBINEROUTPUTNVPROC qglCombinerOutputNV;
-extern PFNGLFINALCOMBINERINPUTNVPROC qglFinalCombinerInputNV;
-extern PFNGLGETCOMBINERINPUTPARAMETERFVNVPROC qglGetCombinerInputParameterfvNV;
-extern PFNGLGETCOMBINERINPUTPARAMETERIVNVPROC qglGetCombinerInputParameterivNV;
-extern PFNGLGETCOMBINEROUTPUTPARAMETERFVNVPROC qglGetCombinerOutputParameterfvNV;
-extern PFNGLGETCOMBINEROUTPUTPARAMETERIVNVPROC qglGetCombinerOutputParameterivNV;
-extern PFNGLGETFINALCOMBINERINPUTPARAMETERFVNVPROC qglGetFinalCombinerInputParameterfvNV;
-extern PFNGLGETFINALCOMBINERINPUTPARAMETERIVNVPROC qglGetFinalCombinerInputParameterivNV;
-
+#include "glNV.h"
 
 // <AWE> : MacOS X 10.2: defined in <OpenGL/glext.h>
 #if !defined (__APPLE__) && !defined (MACOSX) && !defined(SDL) && !defined (__glx__)
@@ -1528,5 +1410,35 @@ void CS_Create(dq3face_t *in, mcurve_t *curve, texture_t *texture);
 void CS_DrawAmbient(mcurve_t *curve);
 void CS_FillBinomials(void);
 
-
 #include "gl_md3.h"
+//*******************************
+//  Shader related declarations
+//*******************************
+//
+
+#include "surfaceflags.h"
+
+//Textures
+#define TEXTURE_RGB 1
+#define TEXTURE_NORMAL 2
+#define TEXTURE_CUBEMAP 3
+
+typedef struct
+{
+	int		texnum;
+	char	identifier[64];
+	int		width, height;
+	qboolean	mipmap;
+	int		type;
+} gltexture_t;
+
+void Draw_TextureMode_f (void);
+
+extern cvar_t gl_nobind;
+extern cvar_t gl_max_size;
+extern cvar_t gl_picmip;
+extern cvar_t gl_gloss;
+extern cvar_t gl_compress_textures;
+extern cvar_t willi_gray_colormaps;
+
+gltexture_t *GL_CacheTexture (char *filename,  qboolean mipmap, int type);
