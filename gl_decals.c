f940252bf79fdc4876de922924c3113d3b684e91
cholleme <cholleme>
2003-02-16 20:08:06 +0000

    no message

--- gl_decals.c
+++ gl_decals.c
@@ -309,10 +309,9 @@ void DecalClipLeaf(decal_t *dec, mleaf_t *leaf)
 		float *v;
 	
 		poly = (*surf)->polys;
-
-		v = (float *)(&globalVertexTable[poly->firstvertex]);
-		for (i=0 ; i<poly->numverts ; i++, v+= VERTEXSIZE)
+		for (i=0 ; i<poly->numverts ; i++)
 		{
+			v = (float *)(&globalVertexTable[poly->firstvertex+i]);
 			newVertex[i][0] = v[0];
 			newVertex[i][1] = v[1];
 			newVertex[i][2] = v[2];
