bca968a59c556a2c7f3cbd7cf44da225b659e180
cholleme <cholleme>
2003-02-16 20:10:09 +0000

    RecursiveHullCheck bug

--- chase.c
+++ chase.c
@@ -33,7 +33,7 @@ vec3_t	chase_dest;
 vec3_t	chase_dest_angles;
 
 // <AWE> missing prototypes
-extern qboolean SV_RecursiveHullCheck (hull_t *hull, int num, float p1f, float p2f, vec3_t p1, vec3_t p2, trace_t *trace);
+extern qboolean SV_RecursiveHullCheck (model_t *m, int num, float p1f, float p2f, vec3_t p1, vec3_t p2, trace_t *trace);
 
 void Chase_Init (void)
 {
@@ -54,7 +54,7 @@ void TraceLine (vec3_t start, vec3_t end, vec3_t impact)
 	trace_t	trace;
 
 	memset (&trace, 0, sizeof(trace));
-	SV_RecursiveHullCheck (cl.worldmodel->hulls, 0, 0, 1, start, end, &trace);
+	SV_RecursiveHullCheck (cl.worldmodel, 0, 0, 1, start, end, &trace);
 
 	VectorCopy (trace.endpos, impact);
 }
