2e8a9573f95f804ab5f73c6965d03d093dd59e58
cholleme <cholleme>
2003-05-04 21:54:20 +0000

    no message

--- draw.h
+++ draw.h
@@ -34,6 +34,7 @@ void Draw_BeginDisc (void);
 void Draw_EndDisc (void);
 void Draw_TileClear (int x, int y, int w, int h);
 void Draw_Fill (int x, int y, int w, int h, int c);
+void Draw_FillRGB (int x, int y, int w, int h, float r, float g, float b);
 void Draw_FadeScreen (void);
 void Draw_String (int x, int y, char *str);
 qpic_t *Draw_PicFromWad (char *name);
