4febae85c63ac3b2d7c2640212b74656ee61acc8
cholleme <cholleme>
2003-11-02 19:56:05 +0000

    Old quake1 screen update code is gone!

--- draw.h
+++ draw.h
@@ -21,14 +21,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // draw.h -- these are the only functions outside the refresh allowed
 // to touch the vid buffer
 
-extern	qpic_t		*draw_disc;	// also used on sbar
-
 void Draw_Init (void);
 void Draw_Character (int x, int y, int num);
 void Draw_DebugChar (char num);
-void Draw_Pic (int x, int y, qpic_t *pic);
-void Draw_TransPic (int x, int y, qpic_t *pic);
-void Draw_TransPicTranslate (int x, int y, qpic_t *pic, byte *translation);
 void Draw_ConsoleBackground (int lines);
 void Draw_BeginDisc (void);
 void Draw_EndDisc (void);
@@ -37,5 +32,3 @@ void Draw_Fill (int x, int y, int w, int h, int c);
 void Draw_FillRGB (int x, int y, int w, int h, float r, float g, float b);
 void Draw_FadeScreen (void);
 void Draw_String (int x, int y, char *str);
-qpic_t *Draw_PicFromWad (char *name);
-qpic_t *Draw_CachePic (char *path);
