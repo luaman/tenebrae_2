6e08585c659afcf83fbdd068c1ac0e4d1bbdede1
cholleme <cholleme>
2003-11-16 22:52:08 +0000

    Added font support

--- draw.h
+++ draw.h
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // draw.h -- these are the only functions outside the refresh allowed
 // to touch the vid buffer
 
+typedef struct drawfont_s drawfont_t ;
+
 void Draw_Init (void);
 void Draw_Character (int x, int y, int num);
 void Draw_DebugChar (char num);
@@ -32,3 +34,8 @@ void Draw_Fill (int x, int y, int w, int h, int c);
 void Draw_FillRGB (int x, int y, int w, int h, float r, float g, float b);
 void Draw_FadeScreen (void);
 void Draw_String (int x, int y, char *str);
+drawfont_t *Draw_FontForName(const char *name);
+drawfont_t *Draw_DefaultFont(void);
+int Draw_StringWidth(char *str, drawfont_t *font);
+int Draw_StringHeight(char *str, drawfont_t *font);
+void Draw_StringFont(int x, int y, char *str, drawfont_t *font);
