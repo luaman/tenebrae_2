834596d0a4ea941048a5aecc24943b6b66616c45
jpaana <jpaana>
2003-11-20 00:44:17 +0000

    Prevent multiple inclusions

--- render.h
+++ render.h
@@ -20,6 +20,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // refresh.h -- public interface to refresh functions
 
+#ifndef __RENDER_H__
+#define __RENDER_H__
+
 #define	MAXCLIPPLANES	11
 
 #define	TOP_RANGE		16			// soldier uniform colors
@@ -183,3 +186,4 @@ void D_DeleteSurfaceCache (void);
 void D_InitCaches (void *buffer, int size);
 void R_SetVrect (vrect_t *pvrect, vrect_t *pvrectin, int lineadj);
 
+#endif
